8586f343c7d6e86f8183486d10fb1ac618b05647
Debojyoti Chakraborty <debojyotichak28@gmail.com>
2025-09-16 23:18:29 +0530

    Adding code outline and documentation for C1M1

--- stats.c
+++ stats.c
@@ -37,39 +37,99 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+                                
+                                
+  printf("Original Data:\n");
+  print_array(test, SIZE);
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  printf("\nSorted Data (Descending):\n");
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+
+  printf("\nStatistics:\n");
+  print_statistics(test, SIZE);
+
+  return 0;
 
 }
 void print_statistics(unsigned char array[], int length) {
-    // TODO: Implement later
+	
+    printf("Minimum: %d\n", find_minimum(array, length));
+    printf("Maximum: %d\n", find_maximum(array, length));
+    printf("Mean   : %d\n", find_mean(array, length));
+    printf("Median : %d\n", find_median(array, length));
 }
 
 void print_array(unsigned char array[], int length) {
-    // TODO: Implement later
+	
+	int i;
+    for (i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 unsigned char find_median(unsigned char array[], int length) {
-    // TODO: Implement later
-    return 0;
+	
+    unsigned char temp[length];
+    int i;
+    for (i = 0; i < length; i++) {
+        temp[i] = array[i];
+    }
+
+    sort_array(temp, length);
+
+    if (length % 2 == 0) {
+        return (temp[length/2 - 1] + temp[length/2]) / 2;
+    } else {
+        return temp[length/2];
+    }
 }
 
 unsigned char find_mean(unsigned char array[], int length) {
-    // TODO: Implement later
-    return 0;
+	
+    unsigned int sum = 0;
+    int i;
+    for (i = 0; i < length; i++) {
+        sum += array[i];
+    }
+    return (unsigned char)(sum / length);
 }
 
 unsigned char find_maximum(unsigned char array[], int length) {
-    // TODO: Implement later
-    return 0;
+	
+    unsigned char max = array[0];
+    int i;
+    for (i = 1; i < length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
 }
 
 unsigned char find_minimum(unsigned char array[], int length) {
-    // TODO: Implement later
-    return 0;
+	
+    unsigned char min = array[0];
+    int i;
+    for (i = 1; i < length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
 }
 
 void sort_array(unsigned char array[], int length) {
-    // TODO: Implement later
+	
+	int i,j;
+    for (i = 0; i < length - 1; i++) {
+        for (j = 0; j < length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                unsigned char temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
